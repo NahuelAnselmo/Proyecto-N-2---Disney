9849f2f0ce2ea292abf714a009078d9498bd587d
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-05-27 10:09:34 -0300

    Avancé con las validaciones del formulario de login

--- js/login/app.js
+++ js/login/app.js
@@ -1,7 +1,15 @@
-const $btnIniciarSesion = document.getElementById('btnIniciarSesion');
+/*const $btnIniciarSesion = document.getElementById('btnIniciarSesion');
 
 $btnIniciarSesion.addEventListener('click', navegarLogin());
 
 function navegarLogin() {
     window.location.href = '../pages/login.html';
+}*/
+import { validateEmail, validatePassword } from '../validators.js';
+
+const $btnConfirmar=document.getElementById('btnConfirmar');
+$btnConfirmar.addEventListener('click', validarCampoMail);
+
+function validarCampoMail() {
+    
 }
