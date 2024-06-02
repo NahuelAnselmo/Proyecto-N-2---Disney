37656aa11a3a59c2f21cdc192362a83196574376
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-06-01 21:02:12 -0300

    Agrego la lógica del login

--- js/login/app.js
+++ js/login/app.js
@@ -1,12 +1,6 @@
 import { validateEmail, validatePassword } from "../validators.js";
-import { estaLogueado } from '../utils.js';
-import { Usuario } from './Usuario.js';
+import { estaLogueado } from './utils.js';
 
-if (estaLogueado()) {
-  window.location.replace('/pages/admin.html');
-}
-
-const usuarioPorDefecto = new Usuario('admin@admin.com', 'admin');
 
 const $inputEmail=document.getElementById('inputEmail');
 const $inputContraseña=document.getElementById('inputContraseña');
@@ -22,30 +16,29 @@ $form.addEventListener('submit', (e) => {
   if (validateEmail($inputEmail, false) && validatePassword($inputContraseña)) {
     const email = $inputEmail.value;
     const contraseña = $inputContraseña.value;
-
-    if (usuarioPorDefecto.validarCredenciales(email, contraseña)) {
-      $alertCredenciales.classList.add('d-none');
-      sessionStorage.setItem('estaLogueado', true);
-      sessionStorage.setItem(
-        'usuario',
-        JSON.stringify({
-          email: usuarioPorDefecto.email,
-          id: usuarioPorDefecto.id,
-        })
-      );
-
+    const logueado = estaLogueado(email, contraseña);
+    if (logueado === true) {
       swal.fire({
-          title: 'Bienvenido',
-          timer: 1500,
-          showCancelButton: false,
-          showConfirmButton: false,
-          timerProgressBar: true,
-        })
-        .then(() => {
-          window.location.assign('/pages/admin.html');
-        });
+        title: 'Bienvenido',
+        timer: 1500,
+        icon: 'success',
+        showCancelButton: false,
+        showConfirmButton: false,
+        timerProgressBar: true,
+      }).then(() => {
+        window.location.assign('/pages/inicio.html');
+      });
     } else {
-      $alertCredenciales.classList.remove('d-none');
+      swal.fire({
+        title: 'Usuario inexistente',
+        icon: 'error',
+        timer: 1500,
+        showCancelButton: false,
+        showConfirmButton: false,
+      }).then(() => {
+        $alertCredenciales.classList.remove('d-none');
+      });
     }
   }
+  $form.reset();
 });
