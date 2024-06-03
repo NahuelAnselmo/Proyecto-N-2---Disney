c42842877936476201f83aff47a262d334e65643
xTucu6 <ivanbalce@gmail.com>
2024-06-03 17:44:44 -0300

    Se agrego el error 404

--- js/registro/Usuario.js
+++
@@ -1,8 +0,0 @@
-export class Usuario{
-    constructor(nombre, email, password){
-        this.id = window.self.crypto.randomUUID();
-        this.nombre = nombre;
-        this.email = email;
-        this.password =  password;
-    }
-}
--- js/registro/utils.js
+++
@@ -1,5 +0,0 @@
-export const agregarUsuarioALS = (usuario) => {
-    let usuarios = obtenerUsuariosDeLS();
-    usuarios.push(usuario); 
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
-  };
